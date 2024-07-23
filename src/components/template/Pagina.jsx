80bfc5bae48e8c8558b9e44c3bedac709c0d161f
GusFSProg <gustavo.prog40@gmail.com>
2024-07-23 11:25:11 -0300

    pronto pela manha

--- src/components/template/Pagina.jsx
+++ src/components/template/Pagina.jsx
@@ -4,16 +4,22 @@ import { Cabecalho } from './Cabecalho'
 
 // import { PaginaProps } from './Interface'
 
-export function Pagina() {
+export function Pagina(props) {
   return (
     <>
       <div
-        className="flex flex-col items-center 
+        className="flex flex-col items-center
       
       bg-zinc-900 h-20  min-h-screen"
       >
         <Cabecalho />
-        <main className="flex-1 w-[1200px] mx-auto h-100 bg-zinc-600 text-white">MAIN</main>
+        <main
+          className={`flex-1 w-[1200px] mx-auto h-100 bg-zinc-600 text-white ${
+            props.className ?? ''
+          } py-10 `}
+        >
+          MAIN
+        </main>
       </div>
     </>
   )
