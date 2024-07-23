a19b5f9036281819ccfaa44edc7a533b25111450
GusFSProg <gustavo.prog40@gmail.com>
2024-07-23 09:46:32 -0300

    carrinho page

--- src/app/carrinho/page.jsx
+++ src/app/carrinho/page.jsx
@@ -1,16 +1,13 @@
-import { Cabecalho } from '@/components/template/Cabecalho'
+// import { Cabecalho } from '@/components/template/Cabecalho'
+import { Pagina } from '../../components/template/Pagina'
 
 export default function Carrinho() {
   return (
     <>
-      <header
-        className="flex w-full justify-between items-center 
-    
-    bg-zinc-800 h-20 px-10"
-      >
-        <Cabecalho />
+      <header>
+        <Pagina />
+        <div>MEU CARRINHO!!</div>
       </header>
-      <div>MEU CARRINHO!!</div>
     </>
   )
 }
