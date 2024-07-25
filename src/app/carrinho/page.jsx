1b04b111fd2b062a85113dd6ddcd1fa1040fe2d3
GustavoFSProg <gustavo.prog40@gmail.com>
2024-07-25 18:45:42 -0300

    carrinho feito mas sem o total

--- src/app/carrinho/page.jsx
+++ src/app/carrinho/page.jsx
@@ -1,37 +1,32 @@
-'use client'
+"use client";
 
-import { useContext, useEffect, useState } from 'react'
-import { Pagina } from '../../components/template/Pagina'
-import { CartContext, CartContextProvider } from '@/data/contexts/CartContext'
-import ProductCard from '@/components/produto/ProductCardCarrinho'
-import ProductCardCarrinho from '@/components/produto/ProductCardCarrinho'
-import TotalCarrinho from '@/components/carrinho/TotalCarrinho'
+import { useContext, useEffect, useState } from "react";
+import { Pagina } from "../../components/template/Pagina";
+import { CartContext, CartContextProvider } from "@/data/contexts/CartContext";
+import ProductCard from "@/components/produto/ProductCardCarrinho";
+import ProductCardCarrinho from "@/components/produto/ProductCardCarrinho";
+import TotalCarrinho from "@/components/carrinho/TotalCarrinho";
+import CarrinhoVazio from "@/components/carrinho/CarrinhoVazio";
 
 export default function Carrinho() {
-  const { number, adds, diminuir, items } = useContext(CartContext)
-  const [data, setData] = useState([])
+  const { number, adds, diminuir, items } = useContext(CartContext);
+  const [data, setData] = useState([]);
 
-  console.log(` IOtems carrrinho: ${items.produto}`)
-
+  console.log(` IOtems carrrinho: ${items.produto}`);
 
   function GetDataProducts() {
-    setData(items.produto)
-    console.log(`items: ${items}`)
+    setData(items.produto);
+    console.log(`items: ${items}`);
 
-    return console.log(`data: ${data}`)
+    return console.log(`data: ${data}`);
   }
 
-
-  
-
   useEffect(() => {
-    GetDataProducts()
-  }, [items])
-
+    GetDataProducts();
+  }, [items]);
 
   return (
     <>
-
       <div>
         <Pagina>
           <div className="flex flex-col justify-center items-center">
@@ -40,34 +35,34 @@ export default function Carrinho() {
             <br />
             <button onClick={diminuir}>Diminuir</button> */}
 
-            <h1 style={{ fontSize: '30px' }}>CARRINHO DE COMPRAS</h1>
+            <h1 style={{ fontSize: "30px" }}>CARRINHO DE COMPRAS</h1>
             <br />
             <br />
 
-            <div className="flex gap-5 justify-center flex-wrap">
-              {items.map((dados) => {
-                return (
-                  <>
-                    <ProductCardCarrinho
-                      nome={dados.nome}
-                      descricao={dados.descricao}
-                      preco={dados.preco}
-                      imagem={dados.imagem}
-                      item={dados}
-                      // remover={(dados) => removeres(dados.produto)}
-
-
-                    />
-                
-                  </>
-                )
-              })}
-              <TotalCarrinho itens={items} />
-            </div>
-          </div> 
+            {items.length === 0 ? (
+              <CarrinhoVazio />
+            ) : (
+              <div className="flex gap-5 justify-center flex-wrap">
+                {items.map((dados) => {
+                  return (
+                    <>
+                      <ProductCardCarrinho
+                        nome={dados.nome}
+                        descricao={dados.descricao}
+                        preco={dados.preco}
+                        imagem={dados.imagem}
+                        item={dados}
+                        // remover={(dados) => removeres(dados.produto)}
+                      />
+                    </>
+                  );
+                })}
+                <TotalCarrinho itens={items} />
+              </div>
+            )}
+          </div>
         </Pagina>
       </div>
-
     </>
-  )
+  );
 }
