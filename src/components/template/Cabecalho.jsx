2993c7bf6d134cc5e48bfb03e2574bbc4aa36815
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2024-08-01 08:15:21 -0300

    header done

--- src/components/template/Cabecalho.jsx
+++ src/components/template/Cabecalho.jsx
@@ -9,7 +9,7 @@ export function Cabecalho() {
   return (
     <div
     
-      className="flex w-full max-md:w-screen justify-between items-center 
+      className="flex w-full max-md:w-screen pt-3 pb-3 justify-between items-center 
       
     bg-zinc-800 h-20 px-10 max-md:h-auto max-md:pt-5 max-md:pb-4"
     >
