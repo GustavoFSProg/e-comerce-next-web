7e7626cea452805e8ef7218e34d1a2c75b3d96e5
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-21 11:10:01 -0300

    product form

--- src/components/template/Cabecalho.jsx
+++ src/components/template/Cabecalho.jsx
@@ -7,7 +7,9 @@ import { Logo } from "./Logo";
 
 export function Cabecalho() {
   return (
-    <div className="flex w-full max-md:w-screen pt-3 pb-3 justify-between items-center bg-zinc-800 h-20 px-10 max-md:h-auto max-md:pt-5 max-md:pb-4">
+    <div className="flex w-full
+     max-md:w-screen pt-3 pb-3 justify-between items-center
+      bg-zinc-800 h-20 px-10 max-md:h-auto max-md:pt-5 max-md:pb-4 text-white">
       <Logo />
       <Carrinho />
     </div>
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -3,13 +3,42 @@ import '../../app/globals.css'
 
 import { Cabecalho } from "@/components/template/Cabecalho";
 import { Pagina } from "@/components/template/Pagina";
+import { IconPhoneOutgoing } from '@tabler/icons-react';
 import Link from 'next/link';
 
 function CreateProduct() {
   return (
-    <Pagina>
+    <Pagina >
+        <h1 className="text-2xl">CADASTRO DE PRODUTOS!</h1>
+      <div className="text-black mt-20">
+
       
-        <h1>CADASTRO DE PRODUTOS!</h1>
+
+        <form className="flex flex-col h-60 items-center justify-center
+         w-100 ">
+           <input type="file" className='text-white' placeholder="imagem"/>
+           <input type="text"  className="bg-slate-200 w-80 p-3 text-xs mt-5 
+           rounded text-stone-800" placeholder="nome"/>
+
+           <input type="text" 
+           className="bg-slate-200 w-80 p-3 text-xs mt-5 
+           rounded text-stone-800"
+           placeholder="descrição"/>
+
+           <input type="text" 
+           className="bg-slate-200 w-80 p-3 text-xs mt-5 
+           rounded text-stone-800"
+           placeholder="preço"/>
+
+           
+           <button className="flex w-80 items-center justify-center 
+           bg-slate-500 p-3 
+           text-sm mt-5 rounded text-slate-200">
+            CADASTRAR
+           </button>
+        </form>
+           </div>
+
    
   </Pagina>
   );
