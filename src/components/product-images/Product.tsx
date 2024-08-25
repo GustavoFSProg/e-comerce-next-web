8402db82998f2d37e3ade1e8e792febe57dcc0c1
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-25 10:14:58 -0300

    multiupload fixed

--- src/components/product-images/Product.tsx
+++ src/components/product-images/Product.tsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import { AdminScreen } from '../components/AdminScreen'
-import { Input } from '../components/Input'
-import ImageUploader from '../components/ImageUploader'
+// import { useState } from 'react'
+// import { AdminScreen } from '../components/AdminScreen'
+// import { Input } from '../components/Input'
+// import ImageUploader from '../components/ImageUploader'
 
-export const Product = () => {
-  const [price, setPrice] = useState<string>('')
+// export const Product = () => {
+//   const [price, setPrice] = useState<string>('')
 
-  return (
-    <AdminScreen >
-      Cadastrar novo produto
-      {/* <Input
-        type="number"
-        name="price"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-        placeholder="Preço do produto"
-      /> */}
-      Arraste e solte as imagens no campo abaixo
-      <ImageUploader />
-    </AdminScreen>
-  )
-}
+//   return (
+//     <AdminScreen >
+//       Cadastrar novo produto
+//       {/* <Input
+//         type="number"
+//         name="price"
+//         value={price}
+//         onChange={(e) => setPrice(e.target.value)}
+//         placeholder="Preço do produto"
+//       /> */}
+//       Arraste e solte as imagens no campo abaixo
+//       <ImageUploader />
+//     </AdminScreen>
+//   )
+// }
