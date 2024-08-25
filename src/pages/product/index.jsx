31fe9b23d207c149338fc2681c43c201723dd227
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-25 10:19:41 -0300

    multiupload novos

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import { AdminScreen } from "../../components/product-images/AdminScreen";
-import { Input } from "../../components/product-images/Input";
 import ImageUploader from "../../components/product-images/ImageUploader";
 import { Pagina } from "@/components/template/Pagina";
 
 function CreateProduct() {
-  const [price, setPrice] = useState("");
 
   return (
     <Pagina>
