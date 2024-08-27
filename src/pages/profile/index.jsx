b8ed23b661852ce23fff0c080ef46b2d1e8e2e3f
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-27 14:08:50 -0300

    Profile images perfect

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -10,38 +10,35 @@ import Products from '@/data/model/Product'
 import { produtos } from '@/data/model/Products-json'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import { useContext, useEffect, useState } from 'react'
 
-export default function Home() {
+import "../../app/globals.css";
+
+
+export default function Profile() {
   const { number } = useContext(CartContext)
   const [prod, setProd] = useState([])
 
-  const navigate = useRouter()
-
-
-
- async function GetDataProducts() {
-    const {data} = await api.get('/products')
-
+  
+  async function GetImagesProducts() {
+      const produtoId =  sessionStorage.getItem('product-id')
+      const {data} = await api.get(`/products-images/${produtoId}`)
+      
     
-    setProd(data.products)
-  }
+    setProd(data)
 
-  function handleProfile(productId){
-     sessionStorage.setItem('product-id', productId)
-
-     navigate.push('/profile')
+    console.log(prod)
   }
 
   useEffect(() => {
-    GetDataProducts()
-  }, [])
+    GetImagesProducts()
+  },)
+
 
   return (
     <Pagina>
       <Link href="/product" className='ml-6'>
-        PRODUTO
+        PRODUTO 
       </Link>
       <br />
       <br />
@@ -50,26 +47,22 @@ export default function Home() {
       <h1 className="max-md:flex max-md:w-48 max-md:ml-1 max-md:text-2xl text-3xl 
        max-md:w-72 max-md:text-center max-md:justify-center" 
       style={{ paddingLeft: '25px'}}>
-        Loja Virtual de Produtos Eletrônicos</h1>
+        produto profile!</h1>
       <br />
       <main className="flex max-md:-mt-10 flex-col items-center max-md:mr-96 max-md:-ml-36 justify-between p-24">
         <div className="max-md:flex max-md:flex-col max-md:mr-80  md:justify-center flex justify-center md:gap-5 md:flex-wrap">
-          {prod.map((items) => {
-            return (
+         
+                {prod.map((items) => {
+                    return (
+                        <div className='flex jsutify-center flex-col p-5' key={prod.id}>
+                            <p>{items.idProduto}</p>
+                <Image src={items.images} width="200" height="200" />
 
-              <div onClick={() => handleProfile(items.id)} className='flex p-5' key={items.id}>
-
-              <ProductCard
-                nome={items.name}
-                descricao={items.descricao}
-                preco={items.preco}
-                imagem={items.imagem}
-                id={items.id}
-                adicionar={(item => adcionar(item.produto))}
-                />
+             
                 </div>
             )
           })}
+            
         </div>
       </main>
     </Pagina>
